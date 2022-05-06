b4775bbad6292c237e25142d834f2f78d7d379a3
WolfsHollow <65318256+WolfsHollow@users.noreply.github.com>
2022-05-06 16:07:11 -0500

    Update Key.js

--- word-master-react/src/components/Key.js
+++ word-master-react/src/components/Key.js
@@ -1,9 +1,17 @@
-const Key = () => {
+const Key = ({ id, className, setKeyValue }) => {
+
+  const handleClick = (e) => {
+    setKeyValue(e.target.id.toUpperCase());
+  }
+
   return (
-    <div>
-      
-    </div>
+    <div id={id} tabIndex="0" className={className} onClick={handleClick}>
+      {id}
+    </div >
 
   );
 }
+
+Key.defaultProps = { className: "key", }
+
 export default Key
