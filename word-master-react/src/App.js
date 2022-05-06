a1f186202e4c6ffe2253cba495d71cd2ea21c9f0
WolfsHollow <65318256+WolfsHollow@users.noreply.github.com>
2022-05-06 16:05:50 -0500

    Update App.js
    limiting progression of text entry

--- word-master-react/src/App.js
+++ word-master-react/src/App.js
@@ -1,49 +1,82 @@
 import Board from './components/Board';
+import Keyboard from './components/Keyboard';
 import { useState, useEffect } from 'react';
 
 function App() {
 
   const [value, setValue] = useState('');
+  const [lineNum, setLineNum] = useState(6);
+  const [boxNum, setBoxNum] = useState(5);
 
   const [keyValue, setKeyValue] = useState('');
 
   const [currentLineValue, setCurrentLineValue] = useState(0);
   const [currentBoxValue, setCurrentBoxValue] = useState(0);
 
-  const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+  const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  const [board, setBoard] = useState([
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', '']
-  ]);
+  const [board, setBoard] = useState([]);
+
+  const [submitted, setSubmitted] = useState(false);
 
   const getKeyPress = (e) => {
-    if (ALPHABET.includes(e.key)) {
-      setKeyValue(e.key);
+    let input = e.key.toUpperCase();
+    if (ALPHABET.includes(input) || input == "ENTER" || input == "BACKSPACE") {
+      setKeyValue(input);
     }
   }
 
   const getNextBox = () => {
-    if (currentBoxValue == 4) {
+    if (currentBoxValue == boxNum - 1 && submitted == true) {
       setCurrentBoxValue(0);
       setCurrentLineValue(currentLineValue + 1);
+      setSubmitted(false);
+    }
+    else if (currentLineValue > lineNum - 1 || currentBoxValue == boxNum) {
+      console.log('dont change anything');
     }
     else {
       setCurrentBoxValue(currentBoxValue + 1);
     }
   }
 
+  const goToPreviousBox = () => {
+    if (currentBoxValue == 0) {
+
+    }
+    else {
+      setCurrentBoxValue(currentBoxValue - 1);
+    }
+  }
+
+  const submitWord = () => {
+    setSubmitted(true);
+  }
+
   useEffect(() => {
-    console.log('test');
     if (!(keyValue == '')) {
-      board[currentLineValue][currentBoxValue] = keyValue;
-      setBoard(board);
-      getNextBox();
-      setKeyValue('');
+      if (keyValue == "ENTER") {
+        if (currentBoxValue == 5) {
+          submitWord();
+          getNextBox();
+          setKeyValue('');
+        }
+      }
+      else if (keyValue == "<-" || keyValue == "BACKSPACE") {
+        board[currentLineValue][currentBoxValue] = '';
+        setBoard(board);
+        goToPreviousBox();
+        setKeyValue('');
+      }
+      else if (currentBoxValue == 4 && board[currentLineValue][currentBoxValue] != '' && submitted != true) {
+        console.log('last box but occupado');
+      }
+      else {
+        board[currentLineValue][currentBoxValue] = keyValue;
+        setBoard(board);
+        getNextBox();
+        setKeyValue('');
+      }
     }
   }, [keyValue])
 
@@ -52,13 +85,17 @@ function App() {
     return () => { document.removeEventListener('keydown', getKeyPress) };
   }, [])
 
-  useEffect(() => {
-    console.log(currentLineValue, currentBoxValue);
-  }, [currentLineValue, currentBoxValue])
-
   return (
     <>
-      <Board board={board} keyValue={keyValue} currentLineValue={setCurrentLineValue} currentBoxValue={setCurrentBoxValue} />
+      <Board
+        board={board}
+        keyValue={keyValue}
+        lineNum={lineNum}
+        boxNum={boxNum}
+        currentLineValue={setCurrentLineValue}
+        currentBoxValue={setCurrentBoxValue}
+      />
+      <Keyboard setKeyValue={setKeyValue} />
     </>
 
   );
