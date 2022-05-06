8f9ecc80f90c4419999efb486ff819baacd08bd3
WolfsHollow <65318256+WolfsHollow@users.noreply.github.com>
2022-05-06 16:07:33 -0500

    Update Line.js

--- word-master-react/src/components/Line.js
+++ word-master-react/src/components/Line.js
@@ -1,11 +1,20 @@
 import Box from './Box'
 
-const Line = ({ board, line, value, wordNum = 5, currentLineValue, currentBoxValue }) => {
+const Line = ({ board, line, value, wordNum, currentLineValue, currentBoxValue }) => {
 
 
     let boxes = [];
     for (let i = 0; i < wordNum; i++) {
-        boxes.push(<Box board={board} line={line} box={i} key={i} value={value} currentLineValue={currentLineValue} currentBoxValue={currentBoxValue} />)
+        board[line].push('');
+        boxes.push(<Box
+            board={board}
+            line={line}
+            box={i}
+            key={i}
+            value={value}
+            currentLineValue={currentLineValue}
+            currentBoxValue={currentBoxValue}
+        />)
     }
 
     return (
