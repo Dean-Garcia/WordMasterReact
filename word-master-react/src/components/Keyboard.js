d393afcad23343dc680478382debf9a600823c67
WolfsHollow <65318256+WolfsHollow@users.noreply.github.com>
2022-05-06 16:07:22 -0500

    Update Keyboard.js

--- word-master-react/src/components/Keyboard.js
+++ word-master-react/src/components/Keyboard.js
@@ -1,9 +1,35 @@
-const Keyboard = () => {
+import Key from './Key';
+
+const Keyboard = ({ setKeyValue }) => {
+
+  let topRow = 'QWERTYUIOP';;
+  let middleRow = 'ASDFGHJKL';
+  let bottomRow = 'ZXCVBNM';
+  let topRowKeys = [];
+  let middleRowKeys = [];
+  let bottomRowKeys = [];
+
+  for (let i = 0; i < topRow.length; i++) {
+    topRowKeys.push(<Key setKeyValue={setKeyValue} key={topRow[i]} id={topRow[i]} />);
+  }
+  for (let i = 0; i < middleRow.length; i++) {
+    middleRowKeys.push(<Key setKeyValue={setKeyValue} key={topRow[i]} id={middleRow[i]} />);
+  }
+
+  bottomRowKeys.push(<Key className='bigKey' setKeyValue={setKeyValue} key={'Enter'} id={'Enter'} />);
+  for (let i = 0; i < bottomRow.length; i++) {
+    bottomRowKeys.push(<Key setKeyValue={setKeyValue} key={topRow[i]} id={bottomRow[i]} />);
+  }
+  bottomRowKeys.push(<Key className='bigKey' setKeyValue={setKeyValue} key={'<-'} id={'<-'} />);
+
   return (
-    <div>
-      
+    <div id='keyboard' className="keyboardContainer">
+      <div id='topRow' className='row'>{topRowKeys}</div>
+      <div id='middleRow' className='row'>{middleRowKeys}</div>
+      <div id='bottomRow' className='row'>{bottomRowKeys}</div>
     </div>
   )
 }
 
 export default Keyboard
+
