7e8854be0883ef55b4ddfb22aafcc51a90877a5e
WolfsHollow <65318256+WolfsHollow@users.noreply.github.com>
2022-05-09 11:05:12 -0500

    added word validation and shake animation

--- word-master-react/src/components/Line.js
+++ word-master-react/src/components/Line.js
@@ -1,6 +1,11 @@
 import Box from './Box'
+import { useState } from 'react';
+import useUpdateEffect from '../customHooks';
+import { ACTIONS } from '../App';
 
 const Line = ({ board, line, boxNum, dispatch, state, word }) => {
+
+    const [lineClass, setLineClass] = useState('lineContainer');
 
     let boxes = [];
     for (let i = 0; i < boxNum; i++) {
@@ -17,8 +22,22 @@ const Line = ({ board, line, boxNum, dispatch, state, word }) => {
         />)
     }
 
+    useUpdateEffect(() => {
+        if (line === state.currentLine && state.isInvalid) {
+            console.log('invalid word');
+            let className = 'lineContainer shakeWord';
+            setLineClass(className);
+            dispatch({ type: ACTIONS.RESET });
+        }
+        setTimeout(() => {
+            setLineClass('lineContainer');
+        }, 300)
+    }, [state.isInvalid])
+
+
+
     return (
-        <div className='lineContainer'>
+        <div className={lineClass}>
             {boxes}
         </div>
     )
