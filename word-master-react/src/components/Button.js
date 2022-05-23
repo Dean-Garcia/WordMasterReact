204c18ecf2341ef8782f7f0bfbde52c75a6c6010
WolfsHollow <65318256+WolfsHollow@users.noreply.github.com>
2022-05-23 15:59:43 -0500

    Create Button.js

---
+++ word-master-react/src/components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ACTIONS, reducer } from '../App.js';
+
+const Button = ({ name, dispatch, state, close }) => {
+
+    let buttonText = name;
+
+    if (close) {
+        buttonText = 'Close';
+    }
+
+    const handleClick = () => {
+        if (name == 'New Word') {
+            dispatch({ type: ACTIONS.NEWGAME });
+        }
+        else {
+            dispatch({ type: ACTIONS.MENU, payload: name });
+        }
+    }
+
+    return (
+        <button className="button" onClick={handleClick}>
+            {buttonText}
+        </button>
+    )
+}
+
+export default Button
