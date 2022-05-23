502e9c94545201b4b2f2f8062b28e7887c9e0c59
WolfsHollow <65318256+WolfsHollow@users.noreply.github.com>
2022-05-23 16:03:29 -0500

    Update App.js

--- word-master-react/src/App.js
+++ word-master-react/src/App.js
@@ -1,7 +1,10 @@
 import Board from './components/Board';
 import Keyboard from './components/Keyboard';
+import Menus from './components/Menus';
+import Buttons from './components/Buttons';
 import { useState, useEffect, useReducer } from 'react';
 import { wordList, default as completeWordList } from './wordList.js';
+import useUpdateEffect from './customHooks';
 
 const generateWord = () => {
   let randomLetterIndex;
@@ -11,14 +14,13 @@ const generateWord = () => {
     randomLetterIndex = getRandomNumber(0, 24);
     let listLength = wordList[randomLetterIndex].length;
     randomWordIndex = getRandomNumber(0, listLength);
-    console.log(randomLetterIndex, randomWordIndex);
     generatedWord = wordList[randomLetterIndex][randomWordIndex].toUpperCase();
     console.log(generatedWord);
     return generatedWord;
 
   }
   catch (e) {
-    console.log('there was an error?', randomLetterIndex, randomWordIndex, generatedWord);
+    console.log('there was an error getting the word at ', randomLetterIndex, randomWordIndex, generatedWord);
     return generateWord();
   }
 
@@ -41,7 +43,14 @@ export const ACTIONS = {
   CORRECT: 'correct',
   PRESENT: 'present',
   WRONG: 'wrong',
-  INVALID: 'invalid'
+  INVALID: 'invalid',
+  NEWGAME: 'newGame',
+  MENU: 'menu',
+  TOGGLE_COLOR_BLIND_MODE: 'toggleColorBlindMode',
+  TOGGLE_ENDURANCE_MODE: 'toggleEnduranceMode',
+  TOGGLE_HARDMODE: 'toggleHardmode',
+  TOGGLE_MENU: 'toggleMenu',
+  CHANGE_THEME: 'changeTheme'
 }
 
 const BACKSPACE = 'BACKSPACE';
@@ -50,8 +59,9 @@ const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export const reducer = (state, { type, payload }) => {
   switch (type) {
-    case ACTIONS.TEST:
+    case ACTIONS.TEST: //test to check dispatch function
       return state;
+
     case ACTIONS.NEXT_BOX:
       if (payload) {
         return { ...state, currentBox: 0, currentLine: state.currentLine + 1, animation: false }
@@ -62,29 +72,86 @@ export const reducer = (state, { type, payload }) => {
       else {
         return { ...state, currentBox: state.currentBox + 1 };
       }
+
     case ACTIONS.NEXT_LINE:
       return { ...state, currentBox: 0, currentLine: state.currentLine + 1 }
+
     case ACTIONS.PREVIOUS_BOX:
       if (state.currentBox === 0) {
         return state;
       }
       return { ...state, currentBox: state.currentBox - 1 };
-    case ACTIONS.HINTS:
+
+    case ACTIONS.HINTS: // add class to boxes to do animation and change background color
       return { ...state, animation: true };
+
     case ACTIONS.WIN:
       return { ...state, animation: true };
-    case ACTIONS.CORRECT:
+
+    case ACTIONS.CORRECT: // add letters to 'correct' array
       return { ...state, correct: state.correct + payload };
-    case ACTIONS.PRESENT:
+
+    case ACTIONS.PRESENT: // add letters to 'present' array
       return { ...state, present: state.present + payload };
-    case ACTIONS.WRONG:
+
+    case ACTIONS.WRONG: // add letters to not present array
       return { ...state, wrong: state.wrong + payload };
-    case ACTIONS.INVALID:
+
+    case ACTIONS.INVALID: // valid word check
       return { ...state, isInvalid: true }
-    case ACTIONS.RESET:
+
+    case ACTIONS.RESET: // reset classes to be able to do animatinos again
       return { ...state, isInvalid: false }
+
+    case ACTIONS.NEWGAME: // gets new word and resets game board
+      return { currentLine: 0, currentBox: 0, lineNum: 6, boxNum: 5, animation: false, correct: '', present: '', wrong: '', isInvalid: false, reset: !state.reset }
+
+    case ACTIONS.MENU:
+      return { ...state, menuName: payload, toggleSetting: !state.toggleSetting };
+
     default:
       return state;
+  }
+}
+
+export const settingsReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ACTIONS.TOGGLE_MENU:
+      switch (payload) {
+        case 'Seed':
+          if (state.isSeedVisible === true) {
+            return { ...state, isSeedVisible: false }
+          }
+          else {
+            return { ...state, isSeedVisible: true, isSettingsVisible: false, isStatsVisible: false }
+          }
+        case 'Settings':
+          if (state.isSettingsVisible === true) {
+            return { ...state, isSettingsVisible: false }
+          }
+          else {
+            return { ...state, isSeedVisible: false, isSettingsVisible: true, isStatsVisible: false }
+          }
+        case 'Stats':
+          if (state.isStatsVisible === true) {
+            return { ...state, isStatsVisible: false }
+          }
+          else {
+            return { ...state, isSeedVisible: false, isSettingsVisible: false, isStatsVisible: true }
+          }
+        default:
+          console.log('setting not implemented yet?');
+          break;
+      }
+      break;
+    case ACTIONS.TOGGLE_ENDURANCE_MODE:
+      return { ...state, TOGGLE_ENDURANCE_MODE: !state.TOGGLE_ENDURANCE_MODE }
+    case ACTIONS.TOGGLE_COLOR_BLIND_MODE:
+      return { ...state, TOGGLE_COLOR_BLIND_MODE: !state.TOGGLE_COLOR_BLIND_MODE }
+    case ACTIONS.TOGGLE_HARDMODE:
+      return { ...state, TOGGLE_HARDMODE: !state.TOGGLE_HARDMODE }
+    case ACTIONS.CHANGE_THEME:
+      return { ...state, CHANGE_THEME: payload };
   }
 }
 
@@ -92,8 +159,13 @@ function App() {
   const [lineNum, setLineNum] = useState(6);
   const [boxNum, setBoxNum] = useState(5);
 
+  const [settingsState, settingsDispatch] = useReducer(
+    settingsReducer, {
+    isSettingsVisible: false, isSeedVisible: false, isStatsVisible: false, hardmode: false, enduranceMode: false, colorBlindMode: false, theme: 'dark'
+  });
+
   const [state, dispatch] = useReducer(reducer, {
-    currentLine: 0, currentBox: 0, lineNum: 6, boxNum: 5, animation: false, correct: '', present: '', wrong: '', isInvalid: false
+    currentLine: 0, currentBox: 0, lineNum: 6, boxNum: 5, animation: false, correct: '', present: '', wrong: '', isInvalid: false, reset: true, menuName: '', toggleSetting: false
   });
 
   const [keyValue, setKeyValue] = useState('');
@@ -167,8 +239,14 @@ function App() {
     return () => { document.removeEventListener('keydown', getKeyPress) };
   }, [])
 
+  useUpdateEffect(() => {
+    setBoard([]);
+    setWord(generateWord());
+  }, [state.reset])
+
   return (
     <div className="appContainer">
+      <Menus state={state} settingsState={settingsState} dispatch={dispatch} settingsDispatch={settingsDispatch} />
       <Board
         board={board}
         keyValue={keyValue}
@@ -179,8 +257,8 @@ function App() {
         word={word}
       />
       <Keyboard setKeyValue={setKeyValue} state={state} />
+      <Buttons className='buttonContainer' dispatch={dispatch} state={state} />
     </div>
-
   );
 }
 
